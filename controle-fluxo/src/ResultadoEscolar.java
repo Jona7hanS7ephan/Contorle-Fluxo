4fa691074cc0f1143fef526b09cee17b366db0b2
jonathan.stephan <julisses2@gmail.com>
2024-02-21 15:49:41 -0300

    Revert "Condiçao encadeada"
    This reverts commit 17b79a793cd0d9f4691d109992984a42dbd324a4.

--- controle-fluxo/src/ResultadoEscolar.java
+++ controle-fluxo/src/ResultadoEscolar.java
@@ -2,11 +2,18 @@ public class ResultadoEscolar {
     public static void main(String[] args) {
         int nota = 6;
 
+<<<<<<< HEAD
         if (nota >= 7)
             System.out.println("Aprovado!");
         else if (nota >= 5 && nota < 7)
             System.out.println("Recuperação");
         else
             System.out.println("Reprovado!");
+=======
+        if(nota >= 7)
+        System.out.println( "Aprovado!");
+        else
+        System.out.println( "Reprovado!");
+>>>>>>> parent of 17b79a7 (Condiçao encadeada)
     }
 }
